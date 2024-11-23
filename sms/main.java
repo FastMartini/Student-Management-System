a10cac7775618a40165e0b2d9bdf26c129bd1cb5
Diego Martinez <diegommart2004@gmail.com>
2024-11-22 23:44:39 -0500

    bug fixes
    A do-while loop is set to prevent the password process from proceeding if the entered password is incorrect.

--- sms/main.java
+++ sms/main.java
@@ -1,12 +1,14 @@
 package sms;
 import java.util.Scanner;
 import java.sql.Connection;
+import static sms.db_function.isValidPassword;
 
 public class main {
 
     public static void main(String[] args) {
         Connection conn = connect_db.connect(); // Get the database connection
         Scanner scanner = new Scanner(System.in);
+        String password;
 
         // Register student
         System.out.println("Register a new student:");
@@ -16,8 +18,19 @@ public class main {
         String lastName = scanner.nextLine();
         System.out.print("Username: ");
         String username = scanner.nextLine();
-        System.out.print("Password: ");
-        String password = scanner.nextLine();
+
+        do {
+            System.out.println("");
+            System.out.println("Password must be at least 8 characters long and contain an uppercase letter, a lowercase letter, a number, and a special character.");
+            System.out.print("Password: ");
+            password = scanner.nextLine();
+
+            // if password is correct, while loop will break
+            if (isValidPassword((password))) {
+                break;
+            }
+        } while (true);
+
 
         /* Calls the registerStudent method from the db_function class,
            passing conn, firstName, lastName, username, and password. */
